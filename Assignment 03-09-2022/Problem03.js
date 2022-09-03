22593649b91e59d0bc89e3ef199ae3e5cc88b3f4
AJAY <ajay9679339@gmail.com>
2022-09-03 18:12:08 +0530

    second commit from second location

---
+++ Assignment 03-09-2022/Problem03.js
@@ -0,0 +1,4 @@
+// Problem 3 : Given and character if it is a consonant print "Consonant"
+
+let char ="z";
+(char!="a" && char!="e" && char!="i" && char!="o" && char!="u") ? console.log("Consonant") : console.log("Vowel");
---
+++ Assignment 03-09-2022/Problem04.js
@@ -0,0 +1,6 @@
+// Problem 4: Given 3 numbers (all different values), print which is greatest
+let x = 50;
+let y = 70;
+let z = 60;
+
+(x>y && x>z) ? console.log("x is greatest") : (y>x && y>z) ? console.log("y is greatest") : console.log("z is greatest");
